001c0b5603f3c96f2333167fb00df56893a1edd8
Roman Gorshunov <roman.gorshunov@att.com>
2019-07-12 17:34:03 +0200

    Retire project
    This project is no longer maintained. This patch removes content and
    references replacement project in a README file.
    
    Change-Id: I388e09cf7ec3cc7b194da12c9cb3eeaf8239e123

--- tests/unit/test_osclient.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from exporter.osclient import OSClient
-
-
-def test_uninitialized_token_is_invalid():
-    osclient = OSClient(None, None, None, None, None, None, None, None)
-    assert osclient.is_valid_token() is False
